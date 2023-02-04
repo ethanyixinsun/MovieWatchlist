11c942c5dacff41dc150959c1ead837df532308e
yixinsun <yixinsun@andrew.cmu.edu>
2023-02-03 18:49:48 -0800

    feat: user logout

--- routes.py
+++ routes.py
@@ -100,6 +100,14 @@ def login():
     return render_template("login.html", title="Movies Watchlist - Login", form=form)
 
 
+@pages.route("/logout")
+def logout():
+    del session["email"]
+    del session["user_id"]
+
+    return redirect(url_for(".login"))
+
+
 @pages.route("/add", methods=["GET", "POST"])
 @login_required
 def add_movie():
